d44ed41fa6b4775b1e35e1dbc1359ffb398088e2
Sean <s7782519728@gmail.com>
2020-12-10 15:21:52 -0800

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,12 +1,39 @@
+import aiofiles
 import os
 import shutil
 
+
 from virtool_core.utils import compress_file, rm
 from virtool_workflow import startup, step, cleanup
-from virtool_workflow_runtime.db import VirtoolDatabase
-from virtool_workflow.execute import run_subprocess
+from virtool_workflow.storage.paths import data_path, temp_path
+from virtool_workflow import hooks
 
 import utils
+
+
+@startup
+async def clean_up(db, job_params, run_in_executor):
+    """
+    Clean up if the job process encounters an error or is cancelled. Removes the host document from the database
+    and deletes any index files.
+
+    """
+
+    @hooks.on_cancelled
+    @hooks.on_failure
+    async def delete_subtraction():
+        try:
+            await run_in_executor(
+                rm,
+                job_params["subtraction_path"],
+                True
+            )
+        except FileNotFoundError:
+            pass
+
+        await db.subtraction.delete_one({"_id": job_params["subtraction_id"]})
+
+        return "delete_subtraction completed"
 
 
 @startup
@@ -47,93 +74,104 @@ def check_db(job_params, data_path, temp_path):
         )
     })
 
+    return "check_db completed"
+
 
 @step
 async def make_subtraction_dir(job_params, run_in_executor):
+    """
+    Make a directory for the host index files at ``<temp_path>/<subtraction_id>``.
+
+    """
     await run_in_executor(
-        os.mkdir,
-        job_params["temp_subtraction_path"]
+        os.makedirs,
+        job_params["temp_subtraction_path"],
+        exist_ok=True
     )
+
+    return "make_subtraction_dir completed"
 
 
 @step
-async def unpack(job_params, run_in_executor, proc):
+async def unpack(job_params, number_of_processes, run_in_executor):
+    """
+    Unpack the FASTA file if it is gzipped.
+
+    """
     await run_in_executor(
         utils.copy_or_decompress,
         job_params["file_path"],
         job_params["temp_fasta_path"],
-        proc
+        number_of_processes
     )
+    return "unpack completed"
 
 
 @step
-async def set_stats(params, db: VirtoolDatabase):
-    gc, count = await utils.calculate_fasta_gc(params["temp_fasta_path"])
+async def set_stats(job_params, db):
+    """
+    Generate some stats for the FASTA file associated with this job. These numbers include nucleotide distribution,
+    length distribution, and sequence count.
 
-    await db.subtraction.update_one({"_id": params["subtraction_id"]}, {
+    """
+    gc, count = await utils.calculate_fasta_gc(job_params["temp_fasta_path"])
+    await db.subtraction.update_one({"_id": job_params["subtraction_id"]}, {
         "$set": {
             "gc": gc,
             "count": count
         }
     })
 
+    return "set_stats completed"
+
 
 @step
-async def bowtie_build(params, proc, run_subprocess, db: VirtoolDatabase):
+async def bowtie_build(db, job_params, number_of_processes, run_subprocess):
+    """
+    Call *bowtie2-build* to build a Bowtie2 index for the host.
+
+    """
     command = [
         "bowtie2-build",
         "-f",
-        "--threads", proc,
-        params["temp_fasta_path"],
-        params["temp_index_path"]
+        "--threads", str(number_of_processes),
+        job_params["temp_fasta_path"],
+        job_params["temp_index_path"]
     ]
 
     await run_subprocess(command)
 
-    await db.subtraction.update_one({"_id": params["subtraction_id"]}, {
+    await db.subtraction.update_one({"_id": job_params["subtraction_id"]}, {
         "$set": {
             "ready": True
         }
     })
 
+    return "bowtie_build completed"
+
 
 @step
-async def compress(params, run_in_executor, proc):
+async def compress(job_params, number_of_processes, run_in_executor):
     """
-        Compress the subtraction FASTA file for long-term storage and download.
-        """
+    Compress the subtraction FASTA file for long-term storage and download.
+
+    """
     await run_in_executor(
         compress_file,
-        params["temp_fasta_path"],
-        params["temp_fasta_path"] + ".gz",
-        proc
+        job_params["temp_fasta_path"],
+        job_params["temp_fasta_path"] + ".gz",
+        number_of_processes
     )
 
     await run_in_executor(
         rm,
-        params["temp_fasta_path"]
+        job_params["temp_fasta_path"]
     )
 
     await run_in_executor(
         shutil.copytree,
-        params["temp_subtraction_path"],
-        params["subtraction_path"]
+        job_params["temp_subtraction_path"],
+        job_params["subtraction_path"]
     )
 
-
-@cleanup
-async def delete_subtraction(params, run_in_executor, db: VirtoolDatabase):
-    """
-        Clean up if the job process encounters an error or is cancelled. Removes the host document from the database
-        and deletes any index files.
-    """
-    try:
-        await run_in_executor(
-            rm,
-            params["subtraction_path"],
-            True
-        )
-    except FileNotFoundError:
-        pass
-
-    await db.subtraction.delete_one({"_id": params["subtraction_id"]})
+    return "compress completed"
