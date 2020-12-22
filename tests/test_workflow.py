bc930697ad7b259fe0f2fa370acbdbab3eeff424
Sean <s7782519728@gmail.com>
2020-12-22 13:18:07 -0800

    Remove check_db step and related test

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -9,33 +9,6 @@ import workflow
 
 
 @pytest.mark.asyncio
-async def test_check_db():
-    with WorkflowFixtureScope() as scope:
-        scope["job_params"] = {"subtraction_id": "foo", "file_id": "bar.fa.gz"}
-        scope["data_path"] = f"{os.getcwd()}/virtool"
-        scope["temp_path"] = f"{os.getcwd()}/temp"
-
-        bound_function = await scope.bind(workflow.check_db)
-        bound_function()
-
-        assert scope["job_params"] == {
-            "subtraction_id": "foo",
-
-            "file_id": "bar.fa.gz",
-
-            "subtraction_path": os.path.join(f"{os.getcwd()}/virtool", "subtractions", "foo"),
-
-            "temp_subtraction_path": os.path.join(f"{os.getcwd()}/temp", "foo"),
-
-            "file_path": os.path.join(f"{os.getcwd()}/virtool", "files", "bar.fa.gz"),
-
-            "temp_fasta_path": os.path.join(f"{os.getcwd()}/temp", "foo", "subtraction.fa"),
-
-            "temp_index_path": os.path.join(f"{os.getcwd()}/temp", "foo", "reference")
-        }
-
-
-@pytest.mark.asyncio
 async def test_make_subtraction_dir(tmpdir):
     with WorkflowFixtureScope() as scope:
         scope["job_params"] = {"temp_subtraction_path": f"{tmpdir}/foo"}
